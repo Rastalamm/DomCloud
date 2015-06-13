d1f8d6573d86f24cd32240dfa8f1f07da1a46824
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-12 17:19:40 -1000

    Need to work on adding the elements/attributes to the array, the array map should work

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -1,10 +1,23 @@
 //window.onload = function (){
 
 
-
+var masterArr = [];
+var masterList = {};
 
   var mover = function(node){
+
   console.log(node.nodeName);
+  masterArr.push(node.nodeName)
+
+  // for(var j = 0; j < node.attributes.length; j++){
+  //   console.log(node.attributes[j].name);
+  //   masterArr.push(node.attributes[j].name);
+  // }
+
+
+
+
+
     if(node.children.length > 0){
 
       for (var i = 0; i < node.children.length; i++) {
@@ -17,8 +30,21 @@
       return;
     }
 
+  masterArr.map(function(current){
 
-  };
+    var exists = masterList.hasOwnProperty(current);
+
+    if(exists){
+      masterList[current] += 1;
+    }else{
+      masterList[current] = 1;
+    }
+
+  });
+
+
+
+};
 
 
 // mover(document.body);
