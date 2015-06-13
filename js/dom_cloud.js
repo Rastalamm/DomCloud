a18852eb7db0f54bd349da0ebd82c9107bba7b83
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-13 10:47:09 -1000

    notes and formatting add to part1

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -1,84 +1,68 @@
  window.onload = function (){
 
 
-var masterArr = [];
-var masterList = {};
+  var masterArr = [];
+  var masterList = {};
 
   var mover = function(node){
-
-  masterArr.push(node.nodeName)
-
+    //pushes all the node names into the temp array
+    masterArr.push(node.nodeName)
+    //pushes all the attribute names into the temp array, if there are more than one
     for(var j = 0; j < node.attributes.length; j++){
-      console.log(node.attributes[j].name);
-
       masterArr.push(node.attributes[j].name);
     }
 
+    //Recusion.
+    //goes to each sibling if there are no children
     if(node.children.length > 0){
-
+      //Goes to the end of the node children
       for (var i = 0; i < node.children.length; i++) {
-
         mover(node.children[i]);
-
       }
-
     }else{
-      console.log('No more children');
+      //Base case of recursion function
       return;
     }
 
-    masterArr.map(function(current){
-
-      var exists = masterList.hasOwnProperty(current);
-
-      if(exists){
-        masterList[current] += 1;
-
-      }else{
-        masterList[current] = 1;
-
-      }
-
-      masterArr = [];
-
-    });
-
-
-
-
-
   };
-
+  //Calls the above function on the page with the body tag
   mover(document.body);
 
+  //mapping the array of the elements/attributes to an object and counting them
+  masterArr.map(function(current){
+
+    var exists = masterList.hasOwnProperty(current);
+
+    if(exists){
+      masterList[current] += 1;
+    }else{
+      masterList[current] = 1;
+    }
+  });
+
+  //Loops through the mapped object and puts them into arrays to be sorted
   var listSorter = [];
 
   for(var q in masterList){
     listSorter.push([q, masterList[q]])
   }
+  //Sorts the inner arrays by highest count
   listSorter.sort(function(a,b){return  b[1] - a[1]});
 
-
+  //creates a generic div to be placed directly after the body opening
   var node = document.createElement('div');
   node.className = 'most_common_list';
+  //places the newly created div
   node = document.body.insertBefore(node, document.body.firstChild);
 
-
-
+  //loops through the sorted array to add the top 20 highest counts to the newly created div
   for(var p = 0; p < 20; p++){
-
     var divy = document.createElement('div');
     divy.innerHTML = listSorter[p][0];
     divy.style.fontSize = listSorter[p][1] * .1 + 'em';
     divy.id = 'list';
     node.appendChild(divy);
-
   }
-
-
-
-
-
 
 
 };
