838c61df2b9fc4d6077c87d3edb9ccdda8162ae3
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-13 10:59:58 -1000

    removed all the comments

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -4,30 +4,22 @@
   var masterList = {};
 
   var mover = function(node){
-    //pushes all the node names into the temp array
     masterArr.push(node.nodeName)
-    //pushes all the attribute names into the temp array, if there are more than one
     for(var j = 0; j < node.attributes.length; j++){
       masterArr.push(node.attributes[j].name);
     }
-
-    //Recusion.
-    //goes to each sibling if there are no children
     if(node.children.length > 0){
-      //Goes to the end of the node children
       for (var i = 0; i < node.children.length; i++) {
         mover(node.children[i]);
       }
     }else{
-      //Base case of recursion function
       return;
     }
 
   };
-  //Calls the above function on the page with the body tag
+
   mover(document.body);
 
-  //mapping the array of the elements/attributes to an object and counting them
   masterArr.map(function(current){
 
     var exists = masterList.hasOwnProperty(current);
@@ -39,22 +31,19 @@
     }
   });
 
-  //Loops through the mapped object and puts them into arrays to be sorted
   var listSorter = [];
 
   for(var q in masterList){
     listSorter.push([q, masterList[q]])
   }
-  //Sorts the inner arrays by highest count
+
   listSorter.sort(function(a,b){return  b[1] - a[1]});
 
-  //creates a generic div to be placed directly after the body opening
   var node = document.createElement('div');
   node.className = 'most_common_list';
-  //places the newly created div
+
   node = document.body.insertBefore(node, document.body.firstChild);
 
-  //loops through the sorted array to add the top 20 highest counts to the newly created div
   for(var p = 0; p < 20; p++){
     var divy = document.createElement('div');
     divy.innerHTML = listSorter[p][0];
@@ -62,7 +51,4 @@
     divy.id = 'top_20_list';
     node.appendChild(divy);
   }
-
-
 };
-
